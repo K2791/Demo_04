7d4c7021aa0d74741f24443ba785f046531ddae7
K2791 <miro.ahola@gmail.com>
2016-02-03 09:11:53 +0200

    Commit

---
+++ Demo_04/Demo_04/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        //field variables
+        private readonly int maxSpeed = 200;
+        private int speed;
+
+        //Property variables
+        public int Speed {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value <= maxSpeed) speed = value;
+                else
+                {
+                    Console.WriteLine("Too much Speed - set to max!");
+                    speed = maxSpeed;
+                }
+            }
+        }
+    }
+}
